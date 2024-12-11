0507a9e53b7f1e870ef5cd072e80bc0565ef8a98
BobMcmuffin <72299908+BobMcmuffin@users.noreply.github.com>
2024-12-11 16:04:03 +0000

    New Token, add_token()
    New Token created for inverted comma
    add_token() function added to Lexer class

--- Lexer.py
+++ Lexer.py
@@ -1,6 +1,18 @@
 from Token import *
 
+#Create Lexer class and assign self variables when initialised
 class Lexer:
-    def __init__(self, script: str) -> None:
+    def __init__(self, script: str) -> None: 
         self.script: str = script
+        self.index: int = 0
+        self.char: str = ''
+        self.tokens: list[Token] = []
     
+    #Create function to progress character
+    def advance(self) -> None:
+        self.index += 1
+        self.char = self.script[self.index]
+    
+    #Add token to self.tokens
+    def add_token(self, char: str) -> None:
+        self.tokens.append(char)
--- Token.py
+++ Token.py
@@ -1,6 +1,10 @@
+
+#Create Token class which can be used later 
 class Token:
     pass
 
+#Declare Tokens with keyword for language later
 PNT: Token = "print"
 LPA: Token = "("
 RPA: Token = ")"
+ICM: Token = "'"
