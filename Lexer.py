ba6a25c3c07a40af25e710fb4e12259748b346dd
BobMcmuffin <72299908+BobMcmuffin@users.noreply.github.com>
2024-12-12 18:01:42 +0000

    Lexer update
    Added remove_original() to Lexer class - to be removed later on when alternative method found
    Added read_script() to Lexer - tokenises script on a letter level

--- Lexer.py
+++ Lexer.py
@@ -10,9 +10,26 @@ class Lexer:
     
     #Create function to progress character
     def advance(self) -> None:
-        self.index += 1
         self.char = self.script[self.index]
+        self.index += 1
+        
     
     #Add token to self.tokens
     def add_token(self, char: str) -> None:
-        self.tokens.append(char)
+        #Spaces aren't required as tokens so do not get added to self.tokens
+        if char != ' ':
+            self.tokens.append(char)
+        else:
+            pass
+    
+    #Clear original self.char from list - to be removed when alternative method found
+    def remove_original(self) -> None:
+        self.tokens.pop(0)
+
+    #Read self.script and append self.tokens for each char
+    def read_script(self) -> None:
+        self.tokens.append(self.char)
+        while self.index < len(self.script):
+            self.advance()
+            self.add_token(self.char)
+        self.remove_original()
